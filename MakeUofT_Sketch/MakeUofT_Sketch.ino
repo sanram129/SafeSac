6487ca9caa61bfff1af6f20bd53005ac5f48507d
Sanjay R <sanjay.ramnauth@mail.utoronto.ca>
2025-02-16 00:00:06 -0500

    added longer steal delay

--- MakeUofT_Sketch/MakeUofT_Sketch.ino
+++ MakeUofT_Sketch/MakeUofT_Sketch.ino
@@ -16,6 +16,9 @@ double acc_mag(sensors_event_t a) {
   return sqrt(pow(a.acceleration.x, 2) + pow(a.acceleration.y, 2) + pow(a.acceleration.z, 2));
 }
 
+// How long the beep remains after steal detected (ms)
+const int StealDelay = 10000;
+
 // Variables to track acceleration
 double pastAcc = 0.0;
 double currentAcc = 0.0;
@@ -128,6 +131,8 @@ void loop() {
     if (fabs(currentAcc - pastAcc) >= 2) {
         // Bag has moved; set off buzzer
         digitalWrite(BUZZER_1, HIGH);
+
+        delay(StealDelay - 200);
     } else {
         digitalWrite(BUZZER_1, LOW);
     }
